c2e33cb987e99f71ec0a1f34857d9e0dc19cba4b
Efko77 <matavimai.evaldas@gmail.com>
2018-05-14 19:50:30 +0300

    init

---
+++ firstDay/FirstTask.java
@@ -0,0 +1,33 @@
+package firstDay;
+
+import java.util.Scanner;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word;
+        do {
+            System.out.println("Iveskite zodi: ");
+            word = scanner.nextLine();
+            if (word.length() % 2 == 0) {
+                System.out.println("Ivestas zodis " + word + "yra lyginis, rasta raidziu " + cuontLetters(word, 'a'));
+            } else {
+                System.out.println("Ivesas zodis " + word + "yra nelyginis, rasta raidziu  " + cuontLetters(word, 'a'));
+            }
+        } while (!word.equals("pabaiga"));
+    }
+
+
+    private static int cuontLetters(String word, char letter) {
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == letter) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
+
+
---
+++ firstDay/MyFirstCass.java
@@ -0,0 +1,46 @@
+package firstDay;
+
+import java.util.Scanner;
+
+public class MyFirstCass {
+    /*public static void main(String[] args) {
+        firstDay.MyFirstCass myFirstCass = new firstDay.MyFirstCass();
+
+        float ff = 5.6f;
+        myFirstCass.notStaticMethod("Jonas");
+
+        System.out.println(sayHi());
+        myFirstCass.checkThisNumber(20);
+
+        System.out.println("Please insert a number !!!");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int numb = scanner.nextInt();
+            System.out.println("Your number is ->" + numb);
+        } catch (inputMismachException e) {
+
+            System.out.println("Ivedete ne skaiciu");
+        }
+
+        private void notStaticMethod (String name){
+            System.out.println(name);
+        }
+
+        private static String sayHi () {
+
+            return " Hi Andrius";
+        }
+
+
+        private void checkThisNumber ( int numb){
+            if (numb > 10) {
+                System.out.println("Daugiau");
+            } else if (numb > 15) {
+                System.out.println("daugiau uz 15");
+            } else {
+                System.out.println("maziau");
+            }
+
+        }
+    }*/
+}
